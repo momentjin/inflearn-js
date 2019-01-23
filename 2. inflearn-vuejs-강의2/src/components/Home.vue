302df806e27abe39747e09e81f11701f7ca79080
momentjin <momentjin@gmail.com>
2019-01-23 23:24:07 +0900

    axios 연습

--- 2. inflearn-vuejs-강의2/src/components/Home.vue
+++ 2. inflearn-vuejs-강의2/src/components/Home.vue
@@ -3,17 +3,45 @@
         Home
         <div>
             Board List:
-            <ul>
-                <li><router-link to="/b/1">Board1</router-link></li>
-                <li><router-link to="/b/2">Board2</router-link></li>
-            </ul>
+            <div v-if="loading"> Loading ... </div>
+            <div v-else> 
+                <div v-for="b in boards" :key="b.id"> 
+                    {{b}}
+                </div>
+            </div>
         </div>
     </div>    
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    
+    data() {
+        return {
+            loading: false,
+            boards: [],
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            this.loading = true;
+            axios.get('http://localhost:3000/boards')
+                .then(res => {
+                    this.boards = res.data
+                })
+                .catch(res => {
+                    // if res.statue 401
+                    this.$router.replace('/login')
+                })
+                .finally(_=> {
+                    this.loading = false
+                })
+        }
+    }
 }
 </script>
 
